4dd6186dd649017572bf4c64d65071c8f7e4616b
Brendan Baird <brendan.b517@gmail.com>
2023-03-07 13:30:02 -0500

    Added username and password inputs

--- flat-tomatoes-front-back/client/src/LoginForm.js
+++ flat-tomatoes-front-back/client/src/LoginForm.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./loginform.css"
 
 function LoginForm() {
+    const [userInfo, setUserInfo] = useState([]);
+    const username = document.getElementById("username-input");
+    const password = document.getElementById("password-input");
+    function handleSubmit(){
+        setUserInfo([username.value, password.value]);
+    }
+
     return (
     <div>
-        <h1>Login</h1>
-        <label htmlFor="username-input">Username:</label>
-        <input id="username-input" type="text" placeholder="Username"/>
-        <label htmlFor="password-input">Password:</label>
-        <input id="password-input" type="password" placeholder="password"/>
+        <h1>Flat Tomatoes</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username-input">Username:</label>
+            <input id="username-input" type="text" placeholder="Username"/>
+            <label htmlFor="password-input">Password:</label>
+            <input id="password-input" type="password" placeholder="password"/>
+            <input type="submit" value="Login"/>
+        </form>
     </div>
     )
 }
