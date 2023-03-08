1e138149b85ffbe429fa444384a77793d1fa56f6
Brendan Baird <brendan.b517@gmail.com>
2023-03-08 10:42:03 -0500

    Finished Login and Signup components, no CSS yet. Routing in progress

---
+++ flat-tomatoes-front-back/client/src/LoginForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./loginform.css"
+
+function LoginForm([onLoginSubmit]) {
+    return (
+    <div>
+        <h1>Flat-Tomatoes</h1>
+        <form onSubmit={onLoginSubmit}>
+            <label htmlFor="username-input">Username:</label>
+            <input id="username-input" type="text" placeholder="Username"/>
+            <label htmlFor="password-input">Password:</label>
+            <input id="password-input" type="password" placeholder="password"/>
+            <input id="submit-button" type="submit" value="Login"/>
+        </form>
+    </div>
+    )
+}
+
+export default LoginForm;
