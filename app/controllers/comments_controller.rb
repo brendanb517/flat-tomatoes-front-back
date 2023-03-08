06936eb7a69375b505edf1be726df61d4d9e6e31
Reese Marshall <reese.amarshall@gmail.com>
2023-03-07 19:55:35 -0500

    I did nothing here

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
 
     def create 
         comment = Comment.create(comment_params) 
+        #byebug
         if(comment.valid?)
             render json: comment, status: :created
         else 
@@ -52,6 +53,6 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
-        params.permit(:user_id, :movie_id, :text)
+        params.permit(:text)
     end
 end
