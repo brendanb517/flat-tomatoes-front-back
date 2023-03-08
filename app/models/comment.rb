083cb16752bc16c990d2061bf19b43a2a272cd9f
Reese Marshall <reese.amarshall@gmail.com>
2023-03-07 23:06:05 -0500

    added backend routes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :movie 
-    belongs_to :user
+    belongs_to :movie, dependent: :destroy
+    belongs_to :user, dependent: :destroy
 
     validates :text, presence: true
 end
