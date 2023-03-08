50f937174d06aca6067cbd75fbbbc34c45b49857
Brendan Baird <brendan.b517@gmail.com>
2023-03-08 15:20:19 -0500

    Currently renders Movies page (home), working on others

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,81 @@
 import './App.css';
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { Switch, Route } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 import Movies from "./Movies";
 
 function App() {
-    
+    // const [count, setCount] = useState(0);
+    const [loginData, setLoginData] = useState({});
+    const [signupData, setSignupData] = useState({});
+    const [userData, setUserData] = useState({});
 
-  // useEffect(() => {
-  //   fetch("/hello")
-  //     .then((r) => r.json())
-  //     .then((data) => setCount(data.count));
-  // }, []);
+// Brendan: functions to handle the login
+  function handleLoginChange(event){
+    setLoginData({...loginData, [event.target.name]: event.target.value})
+  }
+
+  function handleLoginSubmit(event){
+    event.preventDefault();
+    const userLogin = async function() {
+      const response = await fetch("/loginform", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData)
+      });
+      if (response.ok) {
+        response.json()
+        .then(data => setUserData(data));
+      } else {
+        alert(`Error: ${response.status} ${response.statusText}`);
+      }};
+      userLogin();
+  }
+
+// Brendan: functions to handle the signup
+  function handleSignupChange(event){
+    setSignupData({...signupData, [event.target.name]: event.target.value})
+  }
+
+  function handleSignupSubmit(event){
+    event.preventDefault();
+    const userSignup = async function() {
+      const response = await fetch("/signupform", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupData)
+      });
+      if (response.ok) {
+        response.json()
+        .then(userData => setUserData(userData));
+      } else {
+        alert(`Error: ${response.status} ${response.statusText}`);
+      }};
+      userSignup();
+  }
+
+  // Brendan: routes for the login, signup, and movies pages
   return (
     <div className="App">
-      <Movies />
+      <Switch>
+        <Route exact path="/login">
+          <LoginForm onLoginChange={handleLoginChange} onLoginSubmit={handleLoginSubmit} />
+        </Route>
+        <Route exact path="/signup">
+          <SignupForm onSignupChange={handleSignupChange} onSignupSubmit={handleSignupSubmit} />
+        </Route>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./loginform.css"
 
-function LoginForm([onLoginSubmit]) {
+function LoginForm([ onLoginChange, onLoginSubmit ]) {
     return (
     <div>
         <h1>Flat-Tomatoes</h1>
         <form onSubmit={onLoginSubmit}>
             <label htmlFor="username-input">Username:</label>
-            <input id="username-input" type="text" placeholder="Username"/>
+            <input id="username-input" type="text" placeholder="Username" onChange={onLoginChange}/>
             <label htmlFor="password-input">Password:</label>
-            <input id="password-input" type="password" placeholder="password"/>
+            <input id="password-input" type="password" placeholder="password" onChange={onLoginChange}/>
             <input id="submit-button" type="submit" value="Login"/>
         </form>
     </div>
